01cbfbcb8f2a02347a2941455421fe496742a3d3
Karel Psota <karel.psota@cleveranalytics.com>
2022-09-12 13:00:14 +0200

    map tuning

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 """
-# My first app
-Here's our first attempt at using data to create a table:
+# Streamlit demo app of usage with CleverMaps API
 """
 
 import streamlit as st
 import pandas as pd
 import pydeck as pdk
-from cm_sdk import session, wrappers, dwh
+from cm_sdk import session, wrappers
 
 
 def query_result_to_pd_data(metric_results):
@@ -26,7 +25,7 @@ def draw_map():
     if not st.session_state.poi_subtype_selected:
         return
 
-    properties = ['poi_dwh.name', 'poi_dwh.lat', 'poi_dwh.lng']
+    properties = ['poi_dwh.name', 'poi_dwh.lat', 'poi_dwh.lng', 'poi_dwh.type_name', 'poi_dwh.subtype_name', 'poi_dwh.weight']
     filter_by = [
         {
             "property": "poi_dwh.subtype_name",
@@ -36,22 +35,31 @@ def draw_map():
     ]
 
     query_json = {
-        'properties': properties,
-        'filter_by': filter_by
+        "properties": properties,
+        "filter_by": filter_by
     }
 
-    rows_results = wrappers.query(st.session_state.cm_session, query_json, 1000)
+    rows_results = wrappers.query(st.session_state.cm_session, query_json, 20000)
     data = query_result_to_pd_data(rows_results)
 
     df = pd.DataFrame(data=data)
-    df = df.rename(columns={'poi_dwh_name': 'name', 'poi_dwh_lng': 'lng', 'poi_dwh_lat': 'lat'})
+    df = df.rename(columns={'poi_dwh_name': 'name',
+                            'poi_dwh_lng': 'lng',
+                            'poi_dwh_lat': 'lat',
+                            'poi_dwh_type_name': 'type_name',
+                            'poi_dwh_subtype_name': 'subtype_name',
+                            'poi_dwh_weight': 'weight'})
+
+    df["radius"] = df["weight"].apply(lambda weight: weight+10)
+    color_lookup = pdk.data_utils.assign_random_colors(df['subtype_name'])
+    df['color'] = df.apply(lambda row: color_lookup.get(row['subtype_name']), axis=1)
 
     st.session_state.map_placeholder = st.pydeck_chart(pdk.Deck(
         map_style='mapbox://styles/mapbox/light-v9',
         initial_view_state=pdk.ViewState(
             latitude=50.0698,
             longitude=14.4037,
-            zoom=11,
+            zoom=13,
             pitch=0,
         ),
         layers=[
@@ -59,12 +67,23 @@ def draw_map():
                 'ScatterplotLayer',
                 data=df,
                 get_position=['lng', 'lat'],
-                get_color='[200, 30, 0, 160]',
-                get_radius=50,
+                #get_color='[200, 30, 0, 160]',
+                get_color="color",
+                get_radius="radius",
                 radius_min_pixels=1,
-                radius_max_pixels=100
+                radius_max_pixels=100,
+                pickable=True
             )
-        ]
+        ],
+        tooltip={"text": "POI type: {type_name} \n POI subtype: {subtype_name} \n Name: {name} \n "}
+        # tooltip={
+        #     "html": "<b>adresse:</b> {address}"
+        #             "<br/> <b>mape:</b> {pourc_err}"
+        #             " <br/> <b>count:</b> {flow_value_count} "
+        #             "<br/> <b>prediction:</b> {flow_value_streaming}"
+        #             "<br/> <b>pedestrian ids:</b> {elevationValue}",
+        #     "style": {"color": "white"},
+        # }
     ))
 
 def test():
@@ -81,20 +100,18 @@ def main():
     cm_session = session.Session(project_id, dwh_id, access_token, server_url)
     st.session_state.cm_session = cm_session
 
-    # split_properties = ["poi_dwh.type_name"]
-    # split_properties = ["orp_dwh.nazev"]
-    # metric_results = api_caller.query_metric('pois_sum_metric', properties_names=split_properties)
-    # data = query_result_to_pd_data(metric_results)
-
     st.set_page_config(layout="wide")
 
     st.title('CleverMaps Demo Data App')
 
-    cm_prop_values = dwh.PropertyValues(cm_session)
-    poi_subtypes = cm_prop_values.get_property_values('poi_dwh.subtype_name')
+    poi_subtypes = wrappers.get_property_values(cm_session, 'poi_dwh.subtype_name')
 
-    st.selectbox('Select POI subtype to display', poi_subtypes, index=0, key='poi_subtype_selected')
-    #st.multiselect('Select POI subtype to display', poi_subtypes, key='poi_subtype_selected')
+    #st.selectbox('Select POI subtype to display', poi_subtypes, index=10, key='poi_subtype_selected')
+    st.multiselect('Select POI subtype to display',
+                   poi_subtypes,
+                   key='poi_subtype_selected',
+                   default=["atm", 'bank']
+                   )
 
     map_placeholder = st.empty()
     st.session_state.map_placeholder = map_placeholder
